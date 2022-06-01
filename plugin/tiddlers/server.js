b3336fe86fe6c2d8f619b2cb8c5068bdb450fb02
Maurycy Zarzycki <maurycy@evidentlycube.com>
2022-06-01 08:25:06 +0200

    Certain tiddlers are not syncable

--- plugin/tiddlers/server.js
+++ plugin/tiddlers/server.js
@@ -11,44 +11,60 @@ module-type: startup
     /*global $tw: true */
     "use strict";
 
+    const ignoredPatterns = [
+        '$:/StoryList'
+    ];
+
+    function matchIgnoredPattern(tiddlerTitle) {
+        for (const pattern of ignoredPatterns) {
+            if (typeof pattern === 'string' && tiddlerTitle === pattern) {
+                return true;
+            } else if (pattern.test && pattern.test(tiddlerTitle)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     $tw.hooks.addHook('th-server-command-post-start', (_, nodeServer) => {
         if (!$tw.node) {
             return;
         }
 
-        console.log("POST START");
 
         const ws = require('ws');
         const url = require('url');
         const crypto = require('crypto');
         const wsServer = new ws.WebSocketServer({ noServer: true });
         const clients = [];
-        const modified = new Map();
+        const changedTiddlers = new Map();
 
         $tw.wiki.addEventListener("change",function(changes) {
             const message = {};
             const tiddlerTitles = Object.keys(changes);
             for (const tiddlerTitle of tiddlerTitles) {
+                if (matchIgnoredPattern(tiddlerTitle)) {
+                    continue;
+                }
+
                 const tiddler = $tw.wiki.getTiddler(tiddlerTitle);
                 const hash = tiddler
                     ? crypto.createHash('sha1').update(JSON.stringify(tiddler)).digest('base64')
                     : '<deleted>';
 
-                if (modified.get(tiddlerTitle) === hash) {
+                if (changedTiddlers.get(tiddlerTitle) === hash) {
                     continue;
                 }
 
-                modified.set(tiddlerTitle, hash);
+                changedTiddlers.set(tiddlerTitle, hash);
 
                 message[tiddlerTitle] = {
                     ...changes[tiddlerTitle],
-                    modified: tiddler && tiddler.fields.modified ? tiddler.fields.modified.getTime() : null,
                     tiddler
                 };
             }
 
-            console.log(message);
             const tiddlersJson = JSON.stringify(message);
             for (const client of clients) {
                 client.send(tiddlersJson);
@@ -84,30 +100,4 @@ module-type: startup
             }
         });
     });
-
-    setTimeout(() => {
-        try {
-
-            const ws = require('ws');
-            const server = new ws.WebSocketServer({ port: 8080 });
-            const clients = [];
-            server.on('connection', function connection(ws) {
-                console.log("Client connected");
-
-                ws.on('close', () => {
-                    const index = clients.indexOf(ws);
-
-                    if (index !== -1) {
-                        clients.splice(index, 1);
-                    }
-                });
-
-                ws.on('message', function message(data) {
-                  console.log('received: %s', data);
-                });
-            });
-        } catch (e) {
-            console.log("`ws` is not installed so socket-sync plugin won't run: " + e);
-        }
-    }, 50000000);
 })();
